143d3555463c2f03a69f582ed18b7496cc9c797b
Steven Hoang <hoangbaoduy@gmail.com>
2024-02-01 08:46:56 +0800

    update

---
+++ 01. Multi platform docker image for .NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LdapForNet;
+using LdapForNet.Native;
+
+Console.WriteLine("Hello, World!");
+
+using (var cn = new LdapConnection())
+{
+    cn.Connect("ldap.stdynamo.dev", 389, Native.LdapSchema.LDAP);
+    cn.Bind(LdapForNet.Native.Native.LdapAuthMechanism.SIMPLE,"cn=adminstdyn,dc=stdynamo,dc=dev","vq0Ko|a0XJ0:e+|:TId9");
+}
+
+Console.WriteLine("Done.");
